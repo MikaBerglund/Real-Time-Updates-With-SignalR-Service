a92377b765d549cc370cfcb4e13d6fbcf49bd038
Mika Berglund <mika.berglund@meteoriitti.com>
2019-03-23 18:28:35 +0200

    Added default views.

---
+++ ClientApp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientApp.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
