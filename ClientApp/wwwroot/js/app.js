d2605144fb6153a03f78818fa95e1b92b4e5be42
Mika Berglund <mika.berglund@meteoriitti.com>
2019-03-24 09:07:50 +0200

    Added client script and basic markup.

---
+++ ClientApp/wwwroot/js/app.js
@@ -0,0 +1,34 @@
+﻿
+var hbConnection = new signalR.HubConnectionBuilder().withUrl("/events/heartbeat").build();
+var msgConnection = new signalR.HubConnectionBuilder().withUrl("/events/message").build();
+
+var setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+};
+
+hbConnection.start()
+    .then(() => {
+        setInnerText("heartbeat-status", "OK");
+    })
+    .catch(err => {
+        console.error("Error while starting heartbeat connection.", err);
+        setInnerText("heartbeat-status", "Error");
+    }
+);
+msgConnection.start()
+    .then(() => {
+        setInnerText("message-status", "OK");
+    })
+    .catch(err => {
+        console.error("Error while starting message connection.", err);
+        setInnerText("message-status", "Error");
+    }
+);
+
+hbConnection.on("heartbeat", (name, msg) => {
+    setInnerText("heartbeat", msg);
+});
+
+msgConnection.on("message", (name, msg) => {
+    setInnerText("message", msg);
+});
